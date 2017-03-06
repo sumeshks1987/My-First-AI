8c8b32415d311d5d1f5047740e864eeaf14528e8
System Administrator <root@Sumeshs-MacBook-Pro.local>
2017-03-06 16:07:05 +0530

    Index JS update

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -33,17 +33,30 @@ exports.handle = (client) => {
     }
   })
 
+  const handleGreeting = client.createStep({
+    satisfied() {
+      return false
+    },
+
+    prompt() {
+      client.addResponse('greeting')
+      client.done()
+    }
+  })
+
   client.runFlow({
     classifications: {
       // map inbound message classifications to names of streams
+      greeting: 'greeting'
     },
     autoResponses: {
       // configure responses to be automatically sent as predicted by the machine learning model
     },
     streams: {
+      greeting: handleGreeting,
       main: 'onboarding',
       onboarding: [sayHello],
-      end: [untrained],
+      end: [untrained]
     },
   })
 }
