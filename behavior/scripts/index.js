22c846b6754f6d74a33dbeda64054bbfb4c6ee5b
sumeshks1987 <sumesh.ks@envigo.co.uk>
2017-03-16 14:08:40 +0530

    Index updated

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,14 +1,13 @@
 'use strict'
 
 exports.handle = function handle(client) {
-
-  const collectOptions = client.createStep({
+  const collectCity = client.createStep({
     satisfied() {
-      return false
+      console.log('test')
+      return Boolean(client.getConversationState().weatherCity)
     },
 
     extractInfo() {
-      console.log('test')
      const city = client.getFirstEntityWithRole(client.getMessagePart(), 'city')
       if (city) {
         client.updateConversationState({
@@ -19,16 +18,52 @@ exports.handle = function handle(client) {
     },
 
     prompt() {
-      client.addResponse('provide_options)
+      client.addResponse('prompt/weather_city')
       client.done()
+    },
+  })
+
+  const provideWeather = client.createStep({
+    satisfied() {
+      return false
+    },
+
+    prompt(callback) {
+      const environment = client.getEnvironment()
+      console.log(environment)
+      getCurrentWeather(environment.weatherAPIKey, client.getConversationState().weatherCity.value, resultBody => {
+        if (!resultBody || resultBody.cod !== 200) {
+          console.log('Error getting weather.')
+          callback()
+          return
+        }
+
+        const weatherDescription = (
+          resultBody.weather.length > 0 ?
+          resultBody.weather[0].description :
+          null
+        )
+
+        const weatherData = {
+          temperature: Math.round(resultBody.main.temp),
+          condition: weatherDescription,
+          city: resultBody.name,
+        }
+
+        console.log('sending real weather:', weatherData)
+        client.addResponse('provide_weather/current', weatherData)
+        client.done()
+
+        callback()
+      })
     },
   })
 
   client.runFlow({
     classifications: {},
     streams: {
-      main: 'provide_options',
-      provide_options: [collectOptions]
+      main: 'getWeather',
+      getWeather: [collectCity, provideWeather],
     }
   })
 }
