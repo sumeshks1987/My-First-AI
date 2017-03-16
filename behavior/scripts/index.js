180b10492cc40d018ef92b208eb5d6c6b31858c6
sumeshks1987 <sumesh.ks@envigo.co.uk>
2017-03-16 14:24:00 +0530

    Index updated

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -4,6 +4,7 @@ exports.handle = function handle(client) {
   const collectOption = client.createStep({
     satisfied() {
       console.log('test')
+      console.log(client.getConversationState())
       return Boolean(client.getConversationState().weatherCity)
     },
 
@@ -23,39 +24,14 @@ exports.handle = function handle(client) {
     },
   })
 
-  const provideWeather = client.createStep({
+  const request_audit = client.createStep({
     satisfied() {
       return false
     },
 
     prompt(callback) {
-      const environment = client.getEnvironment()
-      console.log(environment)
-      getCurrentWeather(environment.weatherAPIKey, client.getConversationState().weatherCity.value, resultBody => {
-        if (!resultBody || resultBody.cod !== 200) {
-          console.log('Error getting weather.')
-          callback()
-          return
-        }
-
-        const weatherDescription = (
-          resultBody.weather.length > 0 ?
-          resultBody.weather[0].description :
-          null
-        )
-
-        const weatherData = {
-          temperature: Math.round(resultBody.main.temp),
-          condition: weatherDescription,
-          city: resultBody.name,
-        }
-
-        console.log('sending real weather:', weatherData)
-        client.addResponse('provide_weather/current', weatherData)
-        client.done()
-
-        callback()
-      })
+      client.addResponse('request_audit')
+      client.done()
     },
   })
 
@@ -63,7 +39,7 @@ exports.handle = function handle(client) {
     classifications: {},
     streams: {
       main: 'getOption',
-      getOption: [collectOption, provideWeather],
+      getOption: [collectOption, request_audit],
     }
   })
 }
